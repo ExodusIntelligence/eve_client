4d48f2d25fcf775777d491a002cb9ebf66474651
Angel.Martinez <angel.martinez@exodusintel.com>
2022-03-31 09:51:14 -0400

    Add helper functions

---
+++ vms_client/helper.py
@@ -0,0 +1,31 @@
+import re
+
+
+def notify(status, loglevel, msg):
+    loglevel(msg)
+    return {
+        "data": {},
+        "errmsg": f"{status}: {msg}",
+        "ok": "false",
+    }
+
+
+def verify_email(email):
+    """Verify email's format.
+
+    Args:
+        email: email address.
+
+    Raises:
+        ValueError: If `email` is not a string.
+        ValueError: If `email` format is invalid.
+
+    Returns:
+        bool: True
+    """
+    regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    if type(email) is not str:
+        raise ValueError("Email is not a string.")
+    if not re.fullmatch(regex, email):
+        raise ValueError("Invalid email.")
+    return True
